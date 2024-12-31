1c6becc5f49ad8c8708efaa20e037b54b1890f83
xaenou <xaeno@tutamail.com>
2024-12-31 10:08:03 -0500

    fix dataclass

--- origami_video/media_models.py
+++ origami_video/media_models.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from typing import Optional
 
 
+
 @dataclass
 class YTDLPMetadata:
     url: str
@@ -22,13 +23,18 @@ class StreamMetadata:
 
 
 @dataclass
-class MediaMetadata(YTDLPMetadata, StreamMetadata):
-    url: str = field(init=True)
-    id: str = field(init=True)
+class MediaMetadata:
+    # Non-default arguments (from YTDLPMetadata)
+    url: str
+    id: str
+
+    # Optional arguments (from YTDLPMetadata)
     title: Optional[str] = None
     uploader: Optional[str] = None
     extractor: Optional[str] = None
     ext: Optional[str] = None
+
+    # Optional arguments (from StreamMetadata)
     duration: Optional[float] = None
     width: Optional[int] = None
     height: Optional[int] = None
